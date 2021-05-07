8296a7508e962291a2fe1de130e20d0fd27c2c4a
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2021-05-07 14:49:29 +0530

    Added recursive methods for insert and delete node

--- practice_basic_concepts/LinkedList/LinkedList.cpp
+++ practice_basic_concepts/LinkedList/LinkedList.cpp
@@ -118,6 +118,82 @@ Node *deleteNode(Node *head, int i)
   return head;
 }
 
+int lengthRecursive(Node *head)
+{
+  /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input is handled automatically.
+     */
+
+  Node *temp = head;
+  // Base
+  if (temp == NULL)
+  {
+    return 0;
+  }
+
+  int result = length(temp->next);
+
+  result++;
+
+  return result;
+}
+
+Node *insertNodeRec(Node *head, int i, int data)
+{
+  Node *temp = head;
+
+  //Base case
+  if (temp == NULL && i != 0)
+  {
+    return temp;
+  }
+
+  if (i == 0)
+  {
+    Node *new_head = new Node(data);
+    new_head->next = temp;
+
+    return new_head;
+  }
+
+  //Recursion
+  Node *smallOut_head = insertNodeRec(head->next, i - 1, data);
+  temp->next = smallOut_head;
+
+  return temp;
+}
+
+Node *deleteNodeRec(Node *head, int i)
+{
+  /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input is handled automatically.
+     */
+  //Base case
+  if (head == NULL)
+  {
+    return head;
+  }
+
+  if (i == 0)
+  {
+    Node *to_delete = head;
+    head = head->next;
+
+    delete to_delete;
+    return head;
+  }
+
+  //Recursion
+  Node *smallOut_head = deleteNodeRec(head->next, i - 1);
+  head->next = smallOut_head;
+
+  return head;
+}
+
 int main()
 {
   // Node *head = NULL;
@@ -145,11 +221,15 @@ int main()
 
   // printList(head);
 
-  cout << endl;
-  cout << "Enter position index for deletion: " << endl;
-  int pos;
-  cin >> pos;
+  // == Test deleteNode
+  // cout << endl;
+  // cout << "Enter position index for deletion: " << endl;
+  // int pos;
+  // cin >> pos;
 
-  head = deleteNode(head, pos);
-  printList(head);
+  // head = deleteNode(head, pos);
+  // printList(head);
+
+  // == Test: lengthRecursive
+  cout << "lengthRecursive: " << lengthRecursive(head) << endl;
 }
