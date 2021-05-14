5afc42d5e8fdd62874358452043860b07c33d157
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2021-05-14 21:33:25 +0530

    Added numNodes function

--- practice_basic_concepts/Tree/TreeUse.cpp
+++ practice_basic_concepts/Tree/TreeUse.cpp
@@ -104,6 +104,16 @@ void printTreeLevelWise(TreeNode<int> *root)
   }
 }
 
+int numNodes(TreeNode<int> *root)
+{
+  int ans = 1;
+  for (int i = 0; i < root->children.size(); i++)
+  {
+    ans += numNodes(root->children[i]);
+  }
+  return ans;
+}
+
 int main()
 {
   // TreeNode<int> *root = new TreeNode<int>(1);
@@ -118,4 +128,5 @@ int main()
   TreeNode<int> *root = takeInputLevelWise();
   // printTree(root);
   printTreeLevelWise(root);
+  cout << "numNodes: " << numNodes(root) << endl;
 }
